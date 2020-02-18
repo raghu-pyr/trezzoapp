023ccca26f14d7fd854e483c26af4126661fec5d
raghu-pyr <53964187+raghu-pyr@users.noreply.github.com>
2020-02-18 22:24:53 +0530

    Added about and contact page

--- trezzoapp/baseapp/urls.py
+++ trezzoapp/baseapp/urls.py
@@ -4,4 +4,6 @@ from baseapp import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('ml', views.ml, name='ml'),
+    path('about', views.about, name='about'),
+    path('contact', views.contact, name='contact'),
 ]
--- trezzoapp/baseapp/views.py
+++ trezzoapp/baseapp/views.py
@@ -5,5 +5,12 @@ from django.shortcuts import render
 def index(request):
     return render(request, "index.html")
 
+
+def about(request):
+    return render(request, "about.html")
+
+def contact(request):
+    return render(request, "contact.html")
+
 def ml(request):
     return render(request, "mlpage.html") 

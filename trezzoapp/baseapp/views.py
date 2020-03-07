ce4a5f10200394053ca08137832baef26d76d29b
raghu-pyr <53964187+raghu-pyr@users.noreply.github.com>
2020-03-07 23:02:30 +0530

    Removed print messages

--- trezzoapp/baseapp/views.py
+++ trezzoapp/baseapp/views.py
@@ -18,17 +18,10 @@ def contact(request):
     to_addr = ['raghu.pyr@gmail.com']
     if request.method == "POST":
         fname = request.POST['fname']
-        print(fname)
         email = request.POST['email']
-        print(email)
         pnumber = request.POST['pnumber']
-        print(pnumber)
         messagetxt = request.POST['messagetxt']
-        print(messagetxt)
         subject = request.POST['subject']
-        print(subject)
-        #lsname - request.POST['lsname']
-        #print(fname, lsname, email, pnumber, messagetxt)
         send_mail("ContactForm Enquiry: "+subject, messagetxt, from_addr, to_addr)
         return HttpResponseRedirect("")
     return render(request, "contact.html")
